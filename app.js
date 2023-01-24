610d8eac87f5f79c0ab31fd5734eb5dc9b9dda10
Mohammad <moh.hossain98@gmail.com>
2023-01-24 16:19:07 -0500

    comparing with js objects

--- app.js
+++ app.js
@@ -1,4 +1,9 @@
-iphone14 = {
-    brand: "Apple",
-    model: "14 pro"
-}
+const racoon = {
+  name: "Rocket Bob",
+  hasRabies: false,
+  age: 3,
+  sex: "M",
+  number_of_hot_dogs_eaten: 100,
+};
+
+console.log(racoon.name, racoon.age, racoon.sex)
--- app.py
+++ app.py
@@ -1,21 +1,33 @@
-class Smartphone:
+class Racoon:
 
-    def __init__(self, brand, model, color, camera, storage, os):
-        self.brand = brand
-        self.color = color
-        self.camera = camera
-        self.storage = storage
-        self.model = model
-        self._os = os
+    count = 0
+
+    def __init__(self, name, sex, age, number_of_hot_dogs_eaten=0, hasRabies=False):
+        self.name = name
+        self.sex = sex
+        self.age = age
+        self.hasRabies = hasRabies
+        self.number_of_hot_dogs_eaten = number_of_hot_dogs_eaten
+        Racoon.count += 1
 
     def print_info(self):
-        print(f"This is a(n) {self.brand} {self.model} {self.color} which has {self.storage} of storage and a {self.camera} of camera and it runs on {self._os}")
+        print(
+            f"This is {self.name} and this racoon has eaten {self.number_of_hot_dogs_eaten} hot dogs")
+
+    @classmethod
+    def get_count(cls):
+        print(cls.count)
 
 
-iphone14 = Smartphone("Apple", "14 pro", "Gold",
-                      "45 megapixel", "264 gb", "IOS")
+rocket_bob = Racoon("Rocket Bob", "M", 3, False, 100)
 
-print(iphone14.color)
-print(iphone14.model)
+bandit = Racoon("Bandit", "M", 4, 200, True)
 
-iphone14.print_info()
+rocket_bob.name = "Rocket"
+
+print(rocket_bob.name)
+print(rocket_bob.number_of_hot_dogs_eaten)
+
+print(Racoon.get_count())
+
+bandit.print_info()
